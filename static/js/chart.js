2ac00cba2add667beda61111edd4e5434965d187
wcwagner <wcw13@my.fsu.edu>
2017-02-02 18:45:19 -0500

    breaks prices and views into two charts

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,17 +1,20 @@
 $(function () {
     /**
-     * Create the chart when all data is loaded
+     * Create the stock chart when all data is loaded
      * @returns {undefined}
      */
-     var seriesOptions = [], seriesCounter = 0;
+    var stockSeriesOptions = [], stockSeriesCounter = 0;
+    var wikiSeriesOptions = [], wikiSeriesCounter = 0;
 
-    function createChart() {
+    function createStockChart() {
         Highcharts.stockChart('stockchart', {
-
             rangeSelector: {
-                selected: 4
+                selected: 0
             },
-
+            title: {
+                text: 'Daily Stock Price',
+                x: -20
+            },
             yAxis: {
                 labels: {
                     formatter: function () {
@@ -24,30 +27,73 @@ $(function () {
                     color: 'silver'
                 }]
             },
-
             plotOptions: {
                 series: {
                     compare: 'percent',
                     showInNavigator: true
                 }
             },
-
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
             tooltip: {
                 pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
                 valueDecimals: 2,
                 split: true
             },
 
-            series: seriesOptions
+            series: stockSeriesOptions
         });
     }
 
+    function createWikiChart(){
+        Highcharts.stockChart('wikichart', {
+            rangeSelector: {
+                selected: 0
+            },
+            title: {
+                text: 'Weekly Wikipedia Views',
+                x: -20 //center
+            },
+            yAxis: {
+                title: {
+                    text: 'Views'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: wikiSeriesOptions
+        })
+    }
+
     for( var ticker in dailyPrices ){
-        seriesOptions[seriesCounter] = {
+       stockSeriesOptions[stockSeriesCounter] = {
             name: ticker,
             data: dailyPrices[ticker]
         }
-        seriesCounter += 1;
+        stockSeriesCounter += 1;
     }
-    createChart()
+
+    for( var ticker in dailyViews ){
+        wikiSeriesOptions[wikiSeriesCounter] = {
+            name: ticker,
+            data: dailyViews[ticker]
+        }
+        wikiSeriesCounter += 1;
+    }
+
+    createStockChart();
+    createWikiChart();
 });
