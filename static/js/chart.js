a1d865ee896fb405bd7344e22232d1a666e42094
wcwagner <wcw13@my.fsu.edu>
2017-02-05 02:07:08 -0500

    merges two charts back into one

--- static/js/chart.js
+++ static/js/chart.js
@@ -3,9 +3,9 @@ $(function () {
      * Create the stock chart when all data is loaded
      * @returns {undefined}
      */
-    var stockSeriesOptions = [], stockSeriesCounter = 0;
-    var wikiSeriesOptions = [], wikiSeriesCounter = 0;
-
+    var seriesOptions = [], seriesCounter = 0, colorsCounter = 0;
+    colors = ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9',
+   '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1']
     function createStockChart() {
         Highcharts.stockChart('stockchart', {
             rangeSelector: {
@@ -15,7 +15,7 @@ $(function () {
                 text: 'Daily Stock Price',
                 x: -20
             },
-            yAxis: {
+            yAxis: [{ //primary axis -- stock prices
                 labels: {
                     formatter: function () {
                         return (this.value > 0 ? ' + ' : '') + this.value + '%';
@@ -26,74 +26,57 @@ $(function () {
                     width: 2,
                     color: 'silver'
                 }]
-            },
-            plotOptions: {
-                series: {
-                    compare: 'percent',
-                    showInNavigator: true
-                }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            tooltip: {
-                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-                valueDecimals: 2,
-                split: true
-            },
-
-            series: stockSeriesOptions
-        });
-    }
-
-    function createWikiChart(){
-        Highcharts.stockChart('wikichart', {
-            rangeSelector: {
-                selected: 0
-            },
-            title: {
-                text: 'Weekly Wikipedia Views',
-                x: -20 //center
-            },
-            yAxis: {
+            }, { //secondary axis -- wikipedia views
                 title: {
-                    text: 'Views'
+                    text: 'Weekly Wikipedia Views',
+                    x: -20 //center
                 },
                 plotLines: [{
                     value: 0,
                     width: 1,
                     color: '#808080'
                 }]
+            }],
+            plotOptions: {
+                series: {
+                    compare: 'percent',
+                    showInNavigator: true
+                }
+            },
+            tooltip: {
+                shared: true
             },
             legend: {
                 layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
+                align: 'left',
+                x: 80,
+                verticalAlign: 'top',
+                y: 55,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
             },
-            series: wikiSeriesOptions
-        })
+            series: seriesOptions
+        });
     }
 
     for( var ticker in dailyPrices ){
-       stockSeriesOptions[stockSeriesCounter] = {
+       seriesOptions[seriesCounter] = {
             name: ticker,
-            data: dailyPrices[ticker]
+            data: dailyPrices[ticker],
+            color: colors[colorsCounter],
+            yAxis: 0
         }
-        stockSeriesCounter += 1;
-    }
-
-    for( var ticker in dailyViews ){
-        wikiSeriesOptions[wikiSeriesCounter] = {
+        seriesCounter += 1;
+        seriesOptions[seriesCounter] = {
             name: ticker,
-            data: dailyViews[ticker]
+            data: dailyViews[ticker],
+            dashStyle: "Dash",
+            color: colors[colorsCounter],
+            yAxis: 1
         }
-        wikiSeriesCounter += 1;
+        seriesCounter += 1;
+        colorsCounter += 1;
     }
 
     createStockChart();
-    createWikiChart();
 });
