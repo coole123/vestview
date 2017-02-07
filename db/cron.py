b5a9ad01d0e19e92e9bc95ebf3232d14cc3abec3
wcwagner <wcw13@my.fsu.edu>
2017-02-07 17:12:42 -0500

    instantiates conn only once

--- db/cron.py
+++ db/cron.py
@@ -6,19 +6,12 @@ from warnings import filterwarnings
 from yfdaily import insert_daily_snp500
 from wikidaily import insert_daily_snp500_wiki_views
 
-db_host = '127.0.0.1'
-db_user = 'sec_user'
-db_pass = 'password'
-db_name = 'securities_master'
-db_port = 3306
 
-
-def sync_table(table, column):
+def sync_table(conn, table, column):
     """
     Keep all stocks consistent, find the most recent entry for each individual stock.
     Then find the min of those, and delete any row who's price_date is greater.
     """
-    conn = mdb.connect(db_host, db_user, db_pass, db_name, db_port)
     cur = conn.cursor()
     sync = ('SELECT min(grp.max_grped) '
              'FROM (SELECT max({0}) AS max_grped '
@@ -32,8 +25,7 @@ def sync_table(table, column):
 
 
 
-def most_recent_entry_date(table, column):
-    conn = mdb.connect(db_host, db_user, db_pass, db_name, db_port)
+def most_recent_entry_date(conn, table, column):
     cur = conn.cursor()
     cur.execute("SELECT max({0}) AS max_date FROM {1}".format(column, table))
     try:
@@ -42,27 +34,31 @@ def most_recent_entry_date(table, column):
         print("Couldn't find entry in {0} table under {1} column...".format(table, column))
 
 
-def update_daily_prices():
-    conn = mdb.connect(db_host, db_user, db_pass, db_name, db_port)
+def update_daily_prices(conn):
     cur = conn.cursor()
-    sync_table(table='daily_price', column='price_date')
-    most_recent_date = most_recent_entry_date(table='daily_price', column='price_date')
+    sync_table(conn, table='daily_price', column='price_date')
+    most_recent_date = most_recent_entry_date(conn, table='daily_price', column='price_date')
     insert_daily_snp500(start=most_recent_date)
     print("Deleting NULL rows from daily_price table...")
     delnum = cur.execute('DELETE FROM daily_price WHERE adj_close_price IS NULL')
 
 
 
-def update_daily_wiki_views():
-    conn = mdb.connect(db_host, db_user, db_pass, db_name, db_port)
+def update_daily_wiki_views(conn):
     cur = conn.cursor()
-    sync_table(table='daily_wiki_views', column='views_date')
-    most_recent_date = most_recent_entry_date(table='daily_wiki_views', column='views_date')
+    sync_table(conn, table='daily_wiki_views', column='views_date')
+    most_recent_date = most_recent_entry_date(conn, table='daily_wiki_views', column='views_date')
     insert_daily_snp500_wiki_views(start=most_recent_date)
     print("Deleting NULL rows from daily_wiki_views...")
     delnum = cur.execute('DELETE FROM daily_wiki_views WHERE views IS NULL')
 
 
 if __name__ == "__main__":
-    update_daily_wiki_views()
-    update_daily_prices()
+    db_host = '127.0.0.1'
+    db_user = 'sec_user'
+    db_pass = 'password'
+    db_name = 'securities_master'
+    db_port = 3306
+    conn = mdb.connect(db_host, db_user, db_pass, db_name, db_port)
+    update_daily_wiki_views(conn)
+    update_daily_prices(conn)
