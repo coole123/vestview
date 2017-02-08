9042745c9e554ad460ccca62ebb01e748aaa1787
wcwagner <wcw13@my.fsu.edu>
2017-02-07 22:07:27 -0500

    moves credentials out, adds comments

--- db/load_symbols.py
+++ db/load_symbols.py
@@ -1,11 +1,19 @@
+"""
+One-time script to load `symbol` table with data from Wikipeida S&P 500 table
+   https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
+"""
+
 import datetime
 import requests
 import MySQLdb as mdb
 from bs4 import BeautifulSoup
 
+
 def obtain_parse_wiki_snp500():
     """
     Parse the S&P 500 wikipedia table, and return rows corresponding to each company
+
+    Returns List[tuple] : rows corresponding to each row in the Wikipedia table
     """
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = BeautifulSoup(resp.text, 'lxml')
@@ -23,18 +31,11 @@ def obtain_parse_wiki_snp500():
 
     return symbols
 
-def insert_snp500_symbols(symbols):
+
+def insert_snp500_symbols(conn, symbols):
     """
     Insert the rows obtained by `obtain_parse_wiki_snp500 into database
     """
-    db_host = '127.0.0.1'
-    port = 3306
-    db_user = 'sec_user'
-    db_pass = 'password'
-    db_name = 'securities_master'
-    conn = mdb.connect(host=db_host, port=port, user=db_user,
-                       passwd=db_pass, db=db_name)
-
     columns_str = ("ticker, instrument, name, wiki_title, sector, currency,"
                    "created_date, last_updated_date")
     fill_str = "%s, %s, %s, %s, %s, %s, %s, %s"
@@ -42,13 +43,18 @@ def insert_snp500_symbols(symbols):
                           "VALUES ({vals})".format(columns=columns_str,
                                                    vals=fill_str))
 
-    with conn:
-        chunks = [ symbols[i:i+100] for i in range(0, len(symbols), 100) ]
-        cur = conn.cursor()
-        for chunk in chunks:
-            cur.executemany(template_insert_str, chunk)
+    chunks = [ symbols[i:i+100] for i in range(0, len(symbols), 100) ]
+    cur = conn.cursor()
+    for chunk in chunks:
+        cur.executemany(template_insert_str, chunk)
+    cur.close()
 
 
 if __name__ == "__main__":
-    symbols = obtain_parse_wiki_snp500()
-    insert_snp500_symbols(symbols)
+
+    from config import db_host, db_user, db_pass, db_name, db_port
+    conn = mdb.connect(db_host, db_user, db_pass, db_name, db_port)
+    conn.autocommit(True)
+    with conn:
+        symbols = obtain_parse_wiki_snp500()
+        insert_snp500_symbols(conn, symbols)
