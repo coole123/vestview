7d77fb8cc02834579e5f101ec1b907e7142cf519
wcwagner <wcw13@my.fsu.edu>
2017-02-22 11:18:21 -0500

    clears output on courpus notebook

--- db/twitter/tweets.py
+++ db/twitter/tweets.py
@@ -15,26 +15,22 @@ emoticons_str = r"""
         [D\)\]\(\]/\\OpP] # Mouth
     )"""
 
+
 regex_str = [
-    emoticons_str,
-    r'<[^>]+>', # HTML tags
     r'(?:@[\w_]+)', # @-mentions
     r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'\$[A-z]{1,4}', # ticker sybols
     r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
-
     r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
     r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
     r'(?:[\w_]+)', # other words
-    r'(?:\S)' # anything else
 ]
 
 
 tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
-emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
 
-
-def preprocess(s, lowercase=False):
-    tokens = tokens_re.findall(s)
+def tokenize(tweet, lowercase=False):
+    tokens = tokens_re.findall(tweet)
     if lowercase:
         tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
     return tokens
@@ -44,7 +40,7 @@ def preprocess(s, lowercase=False):
 class StdOutListener(StreamListener):
 
     def on_status(self, status):
-        print(preprocess(status.text))
+        print( tokenize(status.text) )
         return True # keep listening
 
     def on_error(self, status):
